6f8be3130c6e8cdad8986430cf01736f6139dc78
Kenny-Miller <47906870+Kenny-Miller@users.noreply.github.com>
2020-03-09 17:03:17 -0600

    Added python scripts

--- backend/dataAccess/addUserAssets.py
+++ backend/dataAccess/addUserAssets.py
@@ -1,18 +1,13 @@
 #! C:/Users/walluser/AppData/Local/Programs/Python/Python38-32/python.exe
 
 
-import cgi, cgitb
-cgitb.enable(display=1, logdir="")
+import json
+import sys
 
-form = cgi.FieldStorage()
+print ("Content-Type: text/json")
+print("")
 
+postBody = sys.stdin.read()
+data = json.loads(postBody)
 
-    
-
-print ("Content-Type: text/html")
-print ("") #use this double quote print statement to add a blank line in the script
-print ("<b>Hello python</b>")
-print(form)
-
-# insert into database
-#print(json.dumps(BL().insertUserAsset(data), default=str))
+print(data)
