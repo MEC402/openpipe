04487e3c6ea0851d27165f93c0f0d81e6298c74c
Kenny-Miller <47906870+Kenny-Miller@users.noreply.github.com>
2020-03-11 17:41:38 -0600

    Got files to work

--- backend/dataAccess/addUserAssets.py
+++ backend/dataAccess/addUserAssets.py
@@ -1,13 +1,19 @@
 #! C:/Users/walluser/AppData/Local/Programs/Python/Python38-32/python.exe
+import cgi, os
+import cgitb; cgitb.enable()
+cgitb.enable(display=1, logdir="C:/Users/walluser/Desktop/logs")
 
 
-import json
-import sys
 
-print ("Content-Type: text/json")
-print("")
 
-postBody = sys.stdin.read()
-data = json.loads(postBody)
+print("Content-Type: text/html")    # HTML is following
+print() 
 
-print(data)
+
+form = cgi.FieldStorage()
+
+fileitem = form["file"]
+if fileitem.file:
+    fn = os.path.basename(fileitem.filename)
+    
+   # open('C:/Users/walluser/Desktop/logs/'+fn, 'wb').write(fileitem.file.read())
