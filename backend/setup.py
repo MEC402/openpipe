96404c1d75315f978e8e2921e080754c1aca6620
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2020-04-09 00:52:00 -0600

    Create setup.py

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# TODO: Complete the information bellow
+
+setup(name='openpipe',
+      version='0.0.1',
+      description='great api',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
