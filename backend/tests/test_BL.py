d8e1556e63ad73500f318137585d9e9d66911bf2
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2020-04-09 02:35:10 -0600

    Update test_BL.py

--- backend/tests/test_BL.py
+++ backend/tests/test_BL.py
@@ -2,7 +2,10 @@ from pythonAPI.OpenPipePy import OpenPipePy
 
 
 def testGetPublicAssets():
-        result=OpenPipePy().getPublicAssetsInFolder(20,1,10)
-        assert ("total" in result)
+    result = OpenPipePy().getPublicAssetsInFolder(20, 1, 10)
+    assert ("total" in result)
 
 
+def testGetAllFolders():
+    result = OpenPipePy().getAllFolders(10)
+    assert ("total" in result)
