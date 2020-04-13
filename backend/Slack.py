c1e031a9d19b0bf1522be6ce052ed3080c9105f1
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2020-04-13 11:12:56 -0600

    Slack Error Handling

---
+++ backend/Slack.py
@@ -0,0 +1,17 @@
+# Posting to a Slack channel
+def sendMessage(text):
+    from urllib import request, parse
+    import json
+
+    post = {"text": "{0}".format(text)}
+
+    try:
+        json_data = json.dumps(post)
+        req = request.Request("https://hooks.slack.com/services/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",
+                              data=json_data.encode('ascii'),
+                              headers={'Content-Type': 'application/json'})
+        resp = request.urlopen(req)
+    except Exception as em:
+        print("EXCEPTION: " + str(em))
+
+
