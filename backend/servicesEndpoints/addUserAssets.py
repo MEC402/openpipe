ec97b7e7d8be047ae5aafac5ced560be278ea2eb
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2020-12-09 06:54:45 -0700

    uploader fix

--- backend/servicesEndpoints/addUserAssets.py
+++ backend/servicesEndpoints/addUserAssets.py
@@ -1,28 +1,50 @@
 #!/bin/python3
+
 import cgi, os
-import cgitb; cgitb.enable()
-from openpipeAPI.ORM.BL import BL
+import cgitb;
+
+cgitb.enable(display=0, logdir="/var/www/cgi-bin/dataAccess/logs/")
+from ORM.BL import BL
+from webdav3.client import Client
 import json
+import Slack
+import traceback
+import sys
+
+try:
+    print("Content-Type: text/json")
+    print()
+
+    form = cgi.FieldStorage()
+
+    fileitem = form["file"]
+    if fileitem is not None and fileitem.file:
+        sourcefileName = os.path.basename(fileitem.filename)
+        ext = sourcefileName.partition('.')[-1]
+        baseURI="http://mec402.boisestate.edu/assets/uploads/"
+        sourceId = "666"
+        folderId = 130
+
+        data={"shortName":sourcefileName,"uri":baseURI,"folderId":folderId,"assetType":ext}
+        uniqueFileName=BL().saveUploadAsset()
+
+        # Save to server
+        path = "../../html/assets/uploads/"
+        open(path + uniqueFileName, 'wb').write(fileitem.file.read())
+        print("{}")
+
+        # Slack.sendMessage("FileUpload Successful")
+except Exception as e:
+    track = traceback.format_exc()
+# Slack.sendMessage(track)
 
 
-print("Content-Type: text/html")
-print()
-
-form = cgi.FieldStorage()
-
-fileitem = form["file"]
-if fileitem is not None and fileitem.file:
-
-    fileName = os.path.basename(fileitem.filename)
-    shortName = fileName.partition('.')[0]
-    uri = "testing123" + fileName
-    idAtSource = 123123
-    sourceId = "666"
-    metaDataId = None
-    scope = 0
-    #Save to server
-    # open(path, 'wb').write(fileitem.file.read())
-
-    #Insert into sql tables
-    BL().insertIntoAsset(shortName, uri, idAtSource, sourceId, metaDataId, scope)
-    print(json.dumps(BL().insertIntoImages(shortName, fileName, uri), default=str))
+# options = {
+#             'webdav_hostname': "http://mec402.boisestate.edu/",
+#             'webdav_root': "webdav",
+#             'webdav_login': "openpipedev",
+#             'webdav_password': "openPipeArtMaster51"
+#         }
+#         client = Client(options)
+#         with open(path + fileName, "rb") as local_file:
+#             client.execute_request(action='upload', path="/user_assets/"+fileName, data=local_file)
