310391f0fd1210261277773283f5085e9e3fa5bf
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2021-09-03 15:51:09 -0600

    added bulk update

--- backend/UsefulCodes/FixModifiedDates.py
+++ backend/UsefulCodes/FixModifiedDates.py
@@ -1,21 +1,41 @@
 from sqlalchemy import and_
 
-from backend.openpipeAPI.ORM.ORM import ORM
-from backend.openpipeAPI.ORM.TO import TO
+from openpipeAPI.ORM.ORM import ORM
+from openpipeAPI.ORM.TO import TO
+
+
+def bulkUpdate(mappings, TableClass, batchSize):
+    updateSize = len(mappings)
+    biteSize = batchSize
+    q = int(updateSize / biteSize)
+    r = updateSize % biteSize
+
+    orm = ORM()
+
+    for i in range(0, q):
+        print("************** commiting to DB **************")
+        orm.session.bulk_update_mappings(TableClass, mappings[i * biteSize:i * biteSize + biteSize])
+        orm.session.flush()
+        orm.session.commit()
+        print("************** Done commiting to DB **************")
+    orm.session.bulk_update_mappings(TableClass, mappings[q * biteSize:q * biteSize + r])
+    orm.commitClose()
 
 
 def fixAssetModifiedDates():
     orm = ORM()
     tables = TO().getClasses()
     Asset = tables["asset"]
-    res = orm.executeSelect("""select asset.id as assetId , max(metaTag.lastModified) as d from asset join metaTag on asset.metaDataId=metaTag.metaDataId group by asset.metaDataId""")
+    mappings = []
+
+    res = orm.executeSelect(
+        """select asset.id as assetId , max(metaTag.lastModified) as d from asset join metaTag on asset.metaDataId=metaTag.metaDataId group by asset.metaDataId""")
 
     for r in res['data']:
         print(r)
-
-        orm.session.query(Asset).filter(Asset.metaDataId == r["assetId"][0]).update({"lastModified": r['d'][0]})
-
-    # print(i,j,i+j)
+        info = {'id': r["assetId"][0], "lastModified": r['d'][0]}
+        mappings.append(info)
+    bulkUpdate(mappings, Asset, 1000)
     orm.commitClose()
 
 
@@ -23,21 +43,19 @@ def fixFolderModifiedDates():
     orm = ORM()
     tables = TO().getClasses()
     Folder = tables["collection"]
-    res = orm.executeSelect("""select collectionMember.collectionId as fid,max(asset.lastModified) as d from asset join collectionMember on asset.id=collectionMember.assetId group by collectionMember.collectionId;""")
+    mapping=[]
+
+    res = orm.executeSelect(
+        """select collectionMember.collectionId as fid,max(asset.lastModified) as d from asset join collectionMember on asset.id=collectionMember.assetId group by collectionMember.collectionId;""")
 
     for r in res['data']:
         print(r)
+        mapping.append({"id":r["fid"][0],"lastModified": r['d'][0]})
 
-        orm.session.query(Folder).filter(Folder.id == r["fid"][0]).update({"lastModified": r['d'][0]})
-
-    # print(i,j,i+j)
+    bulkUpdate(mapping, Folder, 1000)
     orm.commitClose()
-
 
 
 fixAssetModifiedDates()
 
-
 fixFolderModifiedDates()
-
-
