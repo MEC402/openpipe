57bd08fceb66bd9736e689a9bb4fd1b764367d97
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2022-02-19 17:43:28 -0700

    added folder layout lambda

--- AWS_Layer/Database_Access_Layer/BL_Folders.py
+++ AWS_Layer/Database_Access_Layer/BL_Folders.py
@@ -59,3 +59,67 @@ def getFolders(page, pageSize):
     response = {"total": rows, "data": json.loads(folders)}
 
     return response
+
+
+def getFolderLayout(folderId):
+    tables = TO().getClasses()
+    FolderMembers = tables["collectionMember"]
+    MetaTag = tables["metaTag"]
+    Asset = tables["asset"]
+
+    session = da().getSession()
+    # rows = session.query(FolderMembers).filter(FolderMembers.collectionId == folderId).count()
+
+    q1 = session.query(FolderMembers.id, FolderMembers.assetId, FolderMembers.collectionId, FolderMembers.geometry,
+                       FolderMembers.wall, Asset.metaDataId). \
+        filter(FolderMembers.collectionId == folderId, FolderMembers.geometry != None). \
+        join(Asset, Asset.id == FolderMembers.assetId). \
+        subquery()
+
+    q2 = session.query(MetaTag.metaDataId, MetaTag.value). \
+        filter(MetaTag.tagName == 'openpipe_canonical_largeImage'). \
+        subquery()
+
+    resultSet = session.query(q1, q2). \
+        join(q2, q2.c.metaDataId == q1.c.metaDataId, isouter=True). \
+        all()
+
+    session.close()
+
+    layoutData = []
+    for t in resultSet:
+        fmid = t[0]
+        aid = t[1]
+        fid = t[2]
+        geometry = t[3]
+        wall = t[4]
+        mid = t[5]
+        image = t[7]
+
+        layoutData.append({"folderMemberId": fmid,
+                           "assetId": aid,
+                           "folderId": fid,
+                           "geometry": geometry,
+                           "wall": wall,
+                           "assetMetaDataId": mid,
+                           "image": image
+                           })
+
+    response = {"total": len(layoutData), "data": layoutData}
+    return response
+
+
+def deleteFolderAsset(assetId, folderId):
+    tables = TO().getClasses()
+    FolderMembers = tables["collectionMember"]
+
+    result = 0
+    session = da().getSession()
+    result = session.query(FolderMembers).filter(FolderMembers.assetId==assetId,FolderMembers.collectionId==folderId).delete()
+    session.commit()
+    session.close()
+
+    return result
+
+
+print(deleteFolderAsset(2619,25))
