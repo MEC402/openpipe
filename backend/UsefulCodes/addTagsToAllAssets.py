6855a556b565bf3f5936ebb7e1259cd0a2f949ca
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2022-04-09 13:31:03 -0600

    added rating to all assets

--- backend/UsefulCodes/addTagsToAllAssets.py
+++ backend/UsefulCodes/addTagsToAllAssets.py
@@ -1,9 +1,9 @@
-
 import json
 
-from openpipeAPI.ORM.BL import BL
-from openpipeAPI.ORM.ORM import ORM
-from openpipeAPI.ORM.TO import TO
+from backend.openpipeAPI.ORM.BL import BL
+from backend.openpipeAPI.ORM.ORM import ORM
+from backend.openpipeAPI.ORM.TO import TO
+
 
 # orm =ORM()
 # tables = TO().getClasses()
@@ -28,13 +28,29 @@ from openpipeAPI.ORM.TO import TO
 #     orm.session.query(MetaTag).filter(MetaTag.id == int(d["id"][0])).update({"topic_name": "artist", "topic_id": int(d["topic_id"][0])})
 # orm.commitClose()
 
+def add_tag_to_all_assets(tagName, value):
+    orm = ORM()
+    tables = TO().getClasses()
+    MetaTag = tables["metaTag"]
+    res = orm.executeSelect("""SELECT metaDataId FROM asset;""")
+    toInsert = []
 
-orm =ORM()
-tables = TO().getClasses()
-MetaTag = tables["metaTag"]
-res =orm.executeSelect("""SELECT id FROM metaData where tableName='asset';""")
-for r in res['data']:
-    print(r["id"][0])
-    mid = r["id"][0]
-    orm.insert(MetaTag(metaDataId=mid, tagName="openpipe_canonical_moment", value=0))
-orm.commitClose()
+    for r in res['data']:
+        print(r["metaDataId"][0])
+        mid = r["metaDataId"][0]
+        toInsert.append(MetaTag(metaDataId=mid, tagName=tagName, value=value, topic_id=-1))
+
+    insertSize = len(toInsert)
+    biteSize = 1000
+    q = int(insertSize / biteSize)
+    r = insertSize % biteSize
+
+    for i in range(0, q):
+        print(orm.bulkInsert(toInsert[i * biteSize:i * biteSize + biteSize]))
+    orm.bulkInsert(toInsert[q * biteSize:q * biteSize + r])
+    orm.commitClose()
+
+
+tn = "openpipe_canonical_pgRating"
+v = str([0] * 64).strip("[|]|, ").replace(', ', '')
+add_tag_to_all_assets(tn, v)
