0b46244adf28d42ddfa764087e3292401db734e9
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2022-08-31 01:51:21 -0600

    asset change report

--- frontend/src/app/pages/reports/asset-changes/asset-changes.component.ts
+++ frontend/src/app/pages/reports/asset-changes/asset-changes.component.ts
@@ -1,73 +1,116 @@
-import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
-import {LocalDataSource} from "ng2-smart-table";
-import {DataAccessService} from "../../../services/data-access.service";
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
+import {DataAccessService} from '../../../services/data-access.service';
+import {AssetTagCardComponent} from '../../asset/asset-tag-card/asset-tag-card.component';
+import {ActivatedRoute} from '@angular/router';
+import {Observable, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {log} from 'util';
 
 @Component({
   selector: 'ngx-asset-changes',
   templateUrl: './asset-changes.component.html',
-  styleUrls: ['./asset-changes.component.scss']
+  styleUrls: ['./asset-changes.component.scss'],
 })
-export class AssetChangesComponent implements OnInit {
-  ngOnInit() {
-  }
+export class AssetChangesComponent implements OnInit, OnDestroy {
+  @ViewChild(AssetTagCardComponent, { static: false }) assetTagInfo: AssetTagCardComponent;
 
-  settings = {
+
+  totalAssets = 0;
+  folderInfo;
+  changedAssets: LocalDataSource = new LocalDataSource();
+  items = [
+    {title: 'Edit'},
+    {title: 'Delete'},
+  ];
+
+  currentAsset: any;
+  currentAssetType: string;
+  currentAssetFullImage;
+  currentAssetImage: any;
+  showImage = false;
+  loading: boolean;
+
+
+  assetsSettings = {
     actions: {
       add: false,
+      edit: false,
       delete: false,
     },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave : true,
-    },
     columns: {
-      shortName: {
-        title: 'Asset Name',
-        type: 'string',
-        editable: false,
+      openpipe_canonical_smallImage: {
+        filter: false,
+        title: 'Picture',
+        type: 'html',
+        valuePrepareFunction: (openpipe_canonical_smallImage) => {
+          return '<img width="50px" src="' + openpipe_canonical_smallImage[0] + '" />';
+        },
       },
-      tagName: {
-        title: 'Tag Name',
-        type: 'string',
-        editable: false,
-      },
-      value: {
-        title: 'value',
+      openpipe_canonical_title: {
+        title: 'Title',
         type: 'string',
       },
-      sourceName: {
-        title: 'Source Museum',
+      lastModified: {
+        title: 'modified Date',
         type: 'string',
-        editable: false,
       },
     },
   };
 
-  source: LocalDataSource = new LocalDataSource();
+  constructor(private route: ActivatedRoute, private dataAccess: DataAccessService ) { }
 
-  constructor(private dataAccess: DataAccessService,private el: ElementRef) {
-    dataAccess.getAssetsMissingImageReport().subscribe(res => {
-      console.log(res)
-      this.source.load(res.data);
+  private _destroyed$ = new Subject();
+  ngModelDate = new Date();
+
+  ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+
+
+  ngOnInit() {
+    this.onDateChange();
+  }
+
+  onDateChange() {
+    this.changedAssets.empty();
+    const formattedDate = this.ngModelDate.getFullYear() + '-' +
+      (this.ngModelDate.getMonth() + 1) + '-' +
+      this.ngModelDate.getDate();
+
+    console.log(formattedDate);
+    const pageSize = 50;
+    const pages = [];
+
+    this.dataAccess.getChangedAssets(formattedDate, 1, pageSize).pipe(takeUntil(this._destroyed$)).subscribe(res => {
+      this.totalAssets = res.total;
+      for (let i = 2; i <= Math.ceil(res.total / pageSize) ; i += 1) {
+        pages.push(i);
+      }
+      this.changedAssets.load(res.data);
+      Observable.merge(pages.map( g => this.dataAccess.getChangedAssets(formattedDate, g, pageSize)))
+        .subscribe(ob => {
+          ob.pipe(takeUntil(this._destroyed$)).subscribe(resp => {
+            resp.data.forEach(d => {
+              this.changedAssets.add(d).then(p => this.changedAssets.refresh());
+            });
+          });
+        });
     });
   }
 
-
-  onEditConfirm(event) {
-    console.log('edit');
-    this.dataAccess.updateCanonicalMetaTag(event.data.id, event.newData.name);
-    event.confirm.resolve();
+  onClick(event) {
+    this.currentAsset = event.data;
+    this.assetTagInfo.updateUI(this.currentAsset);
+    this.currentAssetType = this.currentAsset.assetType;
+    this.currentAssetImage = this.currentAsset.openpipe_canonical_smallImage[0];
+    this.currentAssetFullImage = this.currentAsset.openpipe_canonical_fullImage[0];
+    console.log(this.currentAsset);
+    this.onFlipCard();
   }
 
-  @HostListener('click') onMouseClick() {
-    console.log('clicked');
-    this.highlight('blue');
-  }
-
-  private highlight(color: string) {
-    console.log(this.el.nativeElement)
-    this.el.nativeElement.style.backgroundColor = color;
+  onFlipCard() {
+    this.showImage = !this.showImage;
   }
 }
